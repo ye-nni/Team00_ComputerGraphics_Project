da2bc26ed41addd5fac73f980d11239dadf49282
ye-nni <sohneun22@gmail.com>
2026-05-28 11:32:22 +0900

    Initial commit

---
+++ Minecraft_Team00/Minecraft.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var gl;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    render();
+};
+
+
+function render() {
+
+}
+
+
